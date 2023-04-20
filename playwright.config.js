ec8bcb36da3da24a33e33ef5b9ed54e5a18c8805
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2023-04-20 17:30:59 +0100

    👷‍♀️ Increase e2e retries on CI
    The e2e tests on GH Actions are currently super flaky. Since we have to
    wait 2 minutes before even running the tests, this change bumps the
    number of retries to 10 to see if they can pass without having to retry
    the whole action.

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ const config = {
     },
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 10 : 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: 'list',
     use: {
